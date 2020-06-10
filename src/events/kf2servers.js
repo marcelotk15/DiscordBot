4673135ae45f15fe0fe379302fd947b52db1e677
BuildTools <unconfigured@null.spigotmc.org>
2020-06-10 12:14:06 -0300

    kf2 server

--- src/events/kf2servers.js
+++ src/events/kf2servers.js
@@ -17,27 +17,27 @@ class KF2Server extends Event{
   async run(){
     //kf2server 1
     setInterval(() => {
-      this.kf2status(['719763242209247283', '719756443024359486'], '35.198.47.172');
+      this.kf2status('Serv. #1', ['719767344221519903', '719756443024359486'], '35.198.47.172');
     }, 15000);
 
     //kf2server 2
     setInterval(() => {
-      this.kf2status(['719767165447831592', '719767192874516503'], '35.198.47.172', '27016');
+      this.kf2status('Serv. #2', ['719767112473772092', '719767192874516503'], '35.198.47.172', '27016');
     }, 15000);
   }
 
-  async kf2status(channels, host, port = null){
+  async kf2status(name, channels, host, port = 27015){
     await gamedig.query({
       type: 'killingfloor2',
       host: host,
-      port: port || '27015'
+      port: port
     }).then(async (data) => {
-      this.client.channels.cache.get(channels[0]).setName('Servidor Online! 🟢');
+      this.client.channels.cache.get(channels[0]).setName(`🟢 ${name} - ${data.map}`);
       this.client.channels.cache.get(channels[1]).setName(`👤 ${data.players.length}/${data.maxplayers}`);
 
     }).catch(err => {
-      this.client.channels.cache.get(channels[0]).setName('Servidor Offline! 🔴');
-      this.client.channels.cache.get(channels[1]).setName('👤 0/0');
+      this.client.channels.cache.get(channels[0]).setName(`🔴 ${name}`);
+      this.client.channels.cache.get(channels[1]).setName('Servidor offline...');
     });
   }
 }
